fc65779c0a160abcbe26250cf4a5f1d2646be0b3
Jane Pascar <japascar@syr.edu>
2019-01-22 17:07:50 -0500

    Update sra-download.sh
    more user friendly date format reporting
    fixed confirming that arguments are supplied

--- sra-download.sh
+++ sra-download.sh
@@ -8,16 +8,18 @@
     # FWD *_1.fastq, REV *_2.fastq, unpaired *.fastq
   # 2. If you redirect the stdout to a .txt file it includes any errors retrieving data from NCBI
   # Also included some info about total runtime
-date
-start=$(date +%s.%N)
+date # start date and time will be reported
+startmin=$(date +%M)
+starthr=$(date +%H)
+startday=$(date +%j)
 
 # check to make sure that both arguments are provided
-if [ $1 == "" ]; then 
+if [ -z $1 ]; then 
 	echo "ERROR: Missing .txt file containing NCBI SRA accession numbers"
     exit 1
 fi
 
-if [ $2 == "" ]; then 
+if [ -z $2 ]; then 
 	echo "ERROR: Missing path to output directory"
 	exit 1
 fi
@@ -28,12 +30,16 @@ if [ "$#" == 2 ]; then
 	DIR=$2
 	while read CUR_ACC; do
 		echo "*** BEGINNING TO DOWNLOAD ${CUR_ACC} ***"
-    	fastq-dump --readids --outdir ${DIR} --origfmt --skip-technical --split-3 ${CUR_ACC}
-    	echo "*** ${CUR_ACC} DOWNLOAD COMPLETE ***"
+    		fastq-dump --readids --outdir ${DIR} --origfmt --skip-technical --split-3 ${CUR_ACC}
+    		echo "*** ${CUR_ACC} DOWNLOAD COMPLETE ***"
 	done <${ACC}
 	echo "*** SCRIPT COMPLETE ***"
-	end=$(date +%s.%N)    
-	runtime=$(python -c "print(${end} - ${start})")
-	echo "Total Time: ${runtime}"
+	endmin=$(date +%M)
+	endhr=$(date +%H)
+	endday=$(date +%j)   
+	runtimemin=$(python -c "print(${endmin} - ${startmin})")
+	runtimehr=$(python -c "print(${endhr} - ${starthr})")
+	runtimeday=$(python -c "print(${endday} - ${startday})")
+	echo "Total Time: ${runtimeday} Day(s), ${runtimehr} Hour(s), ${runtimemin} Minute(s)"
 	exit 0
 fi
